6657fbfaeef2ff42dea922ff14631094ba9449fe
盛向阳 <shengxiangyang@ygsoft.com>
2017-02-07 16:58:26 +0800

    修改后台获取数据服务

--- WN.ProductStore.WebClient/app/core/phone/phone.service.js
+++ WN.ProductStore.WebClient/app/core/phone/phone.service.js
@@ -4,7 +4,7 @@ angular.
   module('core.phone').
   factory('Phone', ['$resource',
     function($resource) {
-      return $resource('phones/:phoneId.json', {}, {
+      return $resource('http://10.52.0.87/ProductStroe/api/Product?pageIndex=0&pageSize=10&name=', {}, {
         query: {
           method: 'GET',
           params: {phoneId: 'phones'},
