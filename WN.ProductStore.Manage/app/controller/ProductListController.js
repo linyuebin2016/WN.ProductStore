70a450b4b1c6f985ed88a720efc4570ba0832631
盛向阳 <shengxiangyang@ygsoft.com>
2017-02-13 09:10:36 +0800

    商品列表修复

--- WN.ProductStore.Manage/app/controller/ProductListController.js
+++ WN.ProductStore.Manage/app/controller/ProductListController.js
@@ -1,7 +1,6 @@
 /**
  * Created by shengxiangyang on 2017-02-09.
  */
-<<<<<<< HEAD
 angular.module('myApp').controller('ProductListController',['$scope','$route', '$log','$http',
     function($scope, $route, $log,$http){
         $scope.name = "dfdfd";
@@ -11,36 +10,6 @@ angular.module('myApp').controller('ProductListController',['$scope','$route', '
                 .success(function (response) {
                     $scope.productList = response.Products;
             });
-=======
-
-angular.module('myApp').controller('ProductListController', ['$scope', '$route', '$log', '$http',
-    function ($scope, $route, $log, $http) {
-        $scope.name = "product!!!";
-        $scope.res = getList();
-
-        function getList() {
-            // return $resource('http://10.52.0.87/ProductStroe/api/Product?pageIndex=0&pageSize=10&name=', {}, {
-            //     query: {
-            //         method: 'GET',
-            //         params: {phoneId: 'phones'},
-            //         isArray: true
-            //     }
-            // });
-
->>>>>>> 99f6b0a9d00d7f55269faf442b4c17ec1664bb1b
         }
-        $scope.num = 0;
-        var products = [{
-            id: 1,
-            name: 'Phone1'
-        }, {
-            id: 2,
-            name: 'Phone2'
-        }, {
-            id: 3,
-            name: 'Phone3'
-        }, ]
-
-        $scope.productList = products;
     }
 ]);
