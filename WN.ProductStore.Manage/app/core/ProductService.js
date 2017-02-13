c93d798f9577cb215dbbed060d7b1cafe7c5be39
盛向阳 <shengxiangyang@ygsoft.com>
2017-02-13 16:37:46 +0800

    框架重构

---
+++ WN.ProductStore.Manage/app/core/ProductService.js
@@ -0,0 +1,23 @@
+define(function (require) {
+    var angular = require('angular');
+    var app = require('../app.config');
+
+    // put into a new module for demo
+    var module = angular.module('product.s.modoule', []);
+
+        module.service('ProductService', function ($http) {
+        return {
+            getProductList: function(){
+                return $http.get("http://10.52.0.87/ProductStroe/api/Product",{
+                    params: {
+                        pageIndex: 0,
+                        pageSize: 10,
+                        name:''
+                    }
+                })
+            }
+        };
+    });
+
+    app.useModule('product.s.modoule');
+});
