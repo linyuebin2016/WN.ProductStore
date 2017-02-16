8431bc3d41f3c5eb1e484397d7161f5f83770b73
林悦斌 <linyuebin@ygsoft.com>
2017-02-16 15:16:25 +0800

    库存管理

--- WN.ProductStore.Manage/app/app-routes.js
+++ WN.ProductStore.Manage/app/app-routes.js
@@ -7,19 +7,19 @@ define(function (require) {
         $rootScope.name = "盛向阳";
     }]);
 
-    app.config(['$stateProvider','$urlRouterProvider',
+    app.config(['$stateProvider', '$urlRouterProvider',
         function config($stateProvider, $urlRouterProvider) {
             /*路由重定向 $urlRouterProvider:如果没有路由引擎能匹配当前的导航状态，那它就会默认将路径路由至 home.html,
              *这个页面就是状态名称被声明的地方. */
             $urlRouterProvider.otherwise('/home');
 
             $stateProvider
-            .state('home', {
-                url: '/home',
-                templateUrl: 'home/home.html',
-                controllerUrl: 'controller/HomeController',
-                controller: 'HomeController'
-            }).
+                .state('home', {
+                    url: '/home',
+                    templateUrl: 'home/home.html',
+                    controllerUrl: 'controller/HomeController',
+                    controller: 'HomeController'
+                }).
             state('product', {
                 url: '/product',
                 templateUrl: 'product/product-list.html',
@@ -33,6 +33,31 @@ define(function (require) {
                 controllerUrl: 'controller/ProductAMController',
                 controller: 'ProductAMController',
                 dependencies: ['core/ProductService']
+            }).
+            state('stockList', {
+                url: '/stockList',
+                templateUrl: 'views/stock/stock-List.html',
+                controllerUrl: 'controller/StockListController',
+                controller: 'StockListController',
+                dependencies: ['core/StockService']
+            }).
+            state('stockIn', {
+                url: '/stockIn',
+                templateUrl: 'views/stock/stockIn.html',
+                controllerUrl: 'controller/StockInController',
+                controller: 'StockInController',
+                dependencies: ['core/StockService']
+            }).
+            state('stockAdd', {
+                url: '/stockAdd/:productId',
+                templateUrl: 'views/stock/stockAdd.html',
+                controllerUrl: 'controller/StockAddController',
+                controller: 'StockAddController',
+                dependencies: ['core/StockService'],
+                params: {
+                    product: null,
+                    stockId: null
+                }
             });
         }
     ]);
--- WN.ProductStore.Manage/app/core/StockService.js
+++ WN.ProductStore.Manage/app/core/StockService.js
@@ -1,16 +1,33 @@
 define(function (require) {
     var angular = require('angular');
     var app = require('../app.config');
-    app.service('StockService', ['$http',function($http) {
+    app.service('StockService', ['$http', function ($http) {
+        var requestUrl = "http://10.52.0.87/ProductStroe/api";
         return {
-            getStockList : function() {
-                return $http.get("http://10.52.0.87/ProductStroe/api/Stock/GetStockList",{
+            getStockList: function () {
+                return $http.get(requestUrl + "/Stock/GetStockList", {
                     params: {
                         pageIndex: 0,
                         pageSize: 10
                     }
                 })
-            }
+            },
+            /**获取产品对应的库存数量 */
+            GetProductStockList: function () {
+                return $http.get(requestUrl + "/Stock/GetProductStockList")
+            },
+
+            /**保存库存 */
+            SaveStock: function (productId, quantity, stockId) {
+                return $http.get(requestUrl + "/Stock/SaveStock", {
+                    params: {
+                        productId: productId,
+                        quantity: quantity,
+                        stockId: stockId
+                    }
+
+                })
+            },
         };
     }]);
 });
