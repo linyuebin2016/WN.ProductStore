45bdffdb8dd87910c1326f20dc8767a77e73a7f5
林悦斌 <linyuebin@ygsoft.com>
2017-02-16 16:10:24 +0800

    库存加入搜索

--- WN.ProductStore/WN.ProductStore/Controllers/StockController.cs
+++ WN.ProductStore/WN.ProductStore/Controllers/StockController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WN.ProductStore.Models;
 using WN.ProductStore.Repository;
 using WN.ProductStore.ViewModel;
 
@@ -14,51 +15,61 @@ namespace WN.ProductStore.Controllers
     {
         DBContext db = new DBContext();
         // GET: api/Stock
-        public StockListView GetStockList(int pageIndex,int pageSize)
+        [HttpPost]
+        public StockListView GetStockList(int pageIndex, int pageSize)
         {
             var view = new StockListView();
-            view.Stocks = db.Stock.OrderByDescending(s=>s.CreateTime).Skip(pageIndex).Take(pageSize).ToList();
+            view.Stocks = db.Stock.OrderByDescending(s => s.CreateTime).Skip(pageIndex).Take(pageSize).ToList();
             view.TotalCount = db.Stock.Count();
             return view;
         }
 
         //获取产品库存关系
-        public object GetProductStockList()
+        [HttpGet]
+        public object GetProductStockList(string queryString)
         {
-            var query = from p in db.Product
-                        join s in db.Stock on p.Id equals s.ProductId
+            IQueryable<Product> queryProduct = db.Product;
+            if (!string.IsNullOrEmpty(queryString))
+                queryProduct = queryProduct.Where(p => p.Name.Contains(queryString) || p.ProductNo.Contains(queryString));
+            var query = from p in queryProduct
+                        join s in db.Stock on p.Id equals s.ProductId into tempS
+                        from tt in tempS.DefaultIfEmpty()
                         select new
                         {
-                            Porduct=p,
-                            Quantity = s.Quantity
+                            Product = p,
+                            Quantity = tt == null ? 0 : tt.Quantity,
+                            StockId = tt == null ? Guid.Empty : tt.Id
                         };
-            return new
+
+            var reslut = new
             {
                 TotalCount = query.Count(),
-                ProductStockList = query
+                ProductStockList = query.ToList()
             };
+            return reslut;
 
         }
 
-        // GET: api/Stock/5
-        public string Get(int id)
+        [HttpGet]
+        public void SaveStock(Guid productId, int quantity, Guid StockId)
         {
-            return "value";
-        }
+            var stock = new Stock();
 
-        // POST: api/Stock
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT: api/Stock/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/Stock/5
-        public void Delete(int id)
-        {
+            if (StockId == Guid.Empty)
+            {
+                stock.Id = Guid.NewGuid();
+                stock.ProductId = productId;
+                stock.Quantity = quantity;
+                db.Stock.Add(stock);
+            }
+            else
+            {
+                var newStock = db.Stock.First(i => i.Id == StockId);
+                newStock.Quantity = newStock.Quantity + quantity;
+                db.Entry(newStock).State = System.Data.Entity.EntityState.Modified;
+            }
+            db.SaveChanges();
         }
     }
 }
